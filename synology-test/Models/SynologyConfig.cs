4dbce6d75555575657cc61bff64d784e1d16e7b0
Mitchell Barry <mitch.barry@gmail.com>
2018-03-17 23:28:40 -0500

    Add synology components

---
+++ synology-test/Models/SynologyConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace synology_test.Models
+{
+    public class SynologyConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string BaseHost { get; set; }
+        public int Port { get; set; }
+        public bool Ssl { get; set; }
+        public int SslPort { get; set; }
+    }
+}
--- synology-test/Startup.cs
+++ synology-test/Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Synology;
+using Synology.Api.Auth.Parameters;
+using Synology.Interfaces;
+using Synology.Settings;
+using synology_test.Models;
 
 namespace synology_test
 {
@@ -22,10 +28,28 @@ namespace synology_test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.Configure<SynologyConfig>(Configuration.GetSection("Synology"));
+
+            services.AddSynology(s =>
+            {
+                s.AddApi();
+                s.AddFileStation();
+            });
+
+            services.AddScoped<ISynologyConnectionSettings, SynologyConnectionSettings>((ctx) =>
+            {
+                var config = ctx.GetService<IOptions<SynologyConfig>>();
+                return ConfigureSynologySettings(config);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ISynologyConnection connection,
+            ISynologyConnectionSettings synologyConnectionSettings)
         {
             if (env.IsDevelopment())
             {
@@ -39,12 +63,30 @@ namespace synology_test
 
             app.UseStaticFiles();
 
+            connection.Api().Auth().Login(new LoginParameters
+            {
+                Username = synologyConnectionSettings.Username,
+                Password = synologyConnectionSettings.Password
+            });
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private SynologyConnectionSettings ConfigureSynologySettings(IOptions<SynologyConfig> synologySettings)
+        {
+            var settings = new SynologyConnectionSettings();
+            settings.BaseHost = synologySettings.Value.BaseHost;
+            settings.Username = synologySettings.Value.Username;
+            settings.Password = synologySettings.Value.Password;
+            settings.Port = synologySettings.Value.Port;
+            settings.Ssl = synologySettings.Value.Ssl;
+            settings.SslPort = synologySettings.Value.SslPort;
+            return settings;
         }
     }
 }
